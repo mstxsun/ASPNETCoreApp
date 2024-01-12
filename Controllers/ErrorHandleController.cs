85266977f7f751cee9656c444143427c702c9d96
Tianxiang Sun <tianxiangsun@microsoft.com>
2024-01-12 12:25:34 -0800

    init dot6 app

---
+++ Controllers/ErrorHandleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet6Application.Controllers
+{
+    public class ErrorHandleController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return StatusCode(200);
+        }
+
+        [HttpGet]
+        public IActionResult TryThrow()
+        {
+            return StatusCode(200);
+        }
+    }
+}
